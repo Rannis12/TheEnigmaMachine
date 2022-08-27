9a663a956ea647ed9fa13b69c223602f91b5ce48
Ran Nissan <rannissan345@gmail.com>
2022-08-27 12:54:19 +0300

    Ran Changes

--- Console/src/uitmp/EnigmaApplication.java
+++ Console/src/uitmp/EnigmaApplication.java
@@ -4,18 +4,24 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.net.URL;
+
 public class EnigmaApplication extends Application {
-
-
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("Hello There in FXML");
 
-        Parent load = FXMLLoader.load(getClass().getResource("tmpFile.fxml"));
-        Scene scene = new Scene(load, 600, 400);
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        URL url = getClass().getResource("../resources/mainPage.fxml");
+        fxmlLoader.setLocation(url);
+        Parent root = fxmlLoader.load(url.openStream());
+
+        Scene scene = new Scene(root, 500, 550);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 }
+
---
+++ Console/src/uitmp/myController.java
@@ -0,0 +1,68 @@
+package uitmp;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+
+public class myController {
+
+    @FXML
+    private BorderPane borderPane;
+
+    @FXML
+    private AnchorPane anchorPaneBD;
+
+    @FXML
+    private VBox titleVB;
+
+    @FXML
+    private Label titleLabel;
+
+    @FXML
+    private GridPane gridPaneTitleVB;
+
+    @FXML
+    private Button loadFileBtn;
+
+    @FXML
+    private Label xmlPathLabel;
+
+    @FXML
+    private AnchorPane centerAP;
+
+    @FXML
+    private TabPane tabPane;
+
+    @FXML
+    private Tab tabOne;
+
+    @FXML
+    private AnchorPane tabOneAP;
+
+    @FXML
+    private Tab tabTwo;
+
+    @FXML
+    private AnchorPane tabTwoAP;
+
+    @FXML
+    private Tab tabThree;
+
+    @FXML
+    private AnchorPane tabThreeAP;
+
+    @FXML
+    void loadFile(MouseEvent event) {
+
+
+
+    }
+
+}
